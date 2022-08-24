6d72672c98a2aa434c19fb0896ebc57250647216
webkida-ss <a5113067.171452@gmail.com>
2022-08-24 22:32:39 +0900

    first commit

---
+++ src/main/java/com/example/springmemogradlejava/SpringMemoGradleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmemogradlejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemoGradleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemoGradleJavaApplication.class, args);
+	}
+
+}
